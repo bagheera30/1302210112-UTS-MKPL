087e0e60c845467094ee14259721c22cf1ecc705
bagheera <r.boeran@gmail.com>
2024-04-21 10:10:24 +0700

    memindahkan variable local menjadi global dan membuat variable ismerried di ambil dari getspouseIdNumber dan perbaikan perhitungan tax

--- src/lib/TaxFunction.java
+++ src/lib/TaxFunction.java
@@ -3,26 +3,18 @@ package lib;
 public class TaxFunction {
 
 	
-	/**
-	 * Fungsi untuk menghitung jumlah pajak penghasilan pegawai yang harus dibayarkan setahun.
-	 * 
-	 * Pajak dihitung sebagai 5% dari penghasilan bersih tahunan (gaji dan pemasukan bulanan lainnya dikalikan jumlah bulan bekerja dikurangi pemotongan) dikurangi penghasilan tidak kena pajak.
-	 * 
-	 * Jika pegawai belum menikah dan belum punya anak maka penghasilan tidak kena pajaknya adalah Rp 54.000.000.
-	 * Jika pegawai sudah menikah maka penghasilan tidak kena pajaknya ditambah sebesar Rp 4.500.000.
-	 * Jika pegawai sudah memiliki anak maka penghasilan tidak kena pajaknya ditambah sebesar Rp 4.500.000 per anak sampai anak ketiga.
-	 * 
-	 */
+	final int deductibleA = 54000000;
+	final int deductibleB = 4500000;
+	final int deductibleC = 1500000;
+	final double roundable = 0.05;
 	
 	
-	 public static int calculateTax(Employment employment, Family family,int numberOfMonthWorking){
+	 public static int calculateTax(Employment employment, Farm family,int numberOfMonthWorking){
 		
 		int tax = 0;
 		int numberOfChildren = family.getChildIdNumbers();
-		final int deductibleA = 54000000;
-		final int deductibleB = 4500000;
-		final int deductibleC = 1500000;
-		final double roundable = 0.05;
+		boolean isMarried=family.getSpouseIdNumber() == "";
+	
 		
 		if (numberOfMonthWorking > 12) {
 			System.err.println("More than 12 month working per year");
@@ -33,7 +25,7 @@ public class TaxFunction {
 		}
 		
 		if (isMarried) {
-			tax = (int) Math.round(roundable * (((monthlySalary + otherMonthlyIncome) * numberOfMonthWorking) - deductible - (deductibleA + deductibleB + (numberOfChildren * deductibleC))));
+			tax = (int) Math.round(roundable * (((employment.getMonthlySalary() + employment.getOtherMonthlyIncome()) * numberOfMonthWorking) - employment.getAnnualDeductible() - (deductibleA + deductibleB + (numberOfChildren * deductibleC))));
 		}else {
 			tax = (int) Math.round(roundable * (((monthlySalary + otherMonthlyIncome) * numberOfMonthWorking) - deductible - deductibleA));
 		}
